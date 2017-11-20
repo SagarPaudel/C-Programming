accdc607c9d8ddc4f2d7b0a90c1d56e098da8abe
Sagar Paudel <33595161+SagarPaudel68@users.noreply.github.com>
2017-11-20 17:11:24 +0545

    Add files via upload

---
+++ Eq1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	float x;
+    float a;
+	x= 2.5;
+	a = 3*x*x*x -5*x*x  + 6;
+	printf("the value of the equation is =%f\n",a);
+	return 0;
+}
---
+++ Eq2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x,a;
+	x=2.5;
+	a= 3* pow(x,3) -5 * pow(x,2) +6;
+	printf("the value of the equation is =%f\n", a);
+	return 0;
+}
