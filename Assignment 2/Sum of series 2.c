f2a77667320123db9d3053b89ef3eadb2d33ea5d
Sagar Paudel <33595161+SagarPaudel@users.noreply.github.com>
2017-11-29 16:58:20 +0545

    Sum of the series
    Sum of the series of any value of x by user input.

--- Assignment 2/Sum of series 2.c
+++ Assignment 2/Sum of series 2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<math.h>
 int main()
+	//sum of the series of x+ x^2 + x^3 ....
 {
-	int i,p=1,x=7,a,z,n;
+	int i,p=1,a,z,n;
+	printf("Enter the value of z =");
 	scanf("%d", &z);
 	for (i=1; i<=z; i++)
 	{
-		n=pow(x,p);
+		n=pow(z,p);
 		a=a+n;
 		p++;
 	}
