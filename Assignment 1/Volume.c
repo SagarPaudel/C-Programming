607eb38e0ad99cc7b23ab1cd1d2a6884eefc6e3d
Sagar Paudel <33595161+SagarPaudel@users.noreply.github.com>
2017-12-02 07:02:00 +0545

    Added #define preprocessor

--- Assignment 1/Volume.c
+++ Assignment 1/Volume.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<math.h>
+#define pi 3.14
 int main()
 {
-	float r, v, pi;
-	pi = 3.14;
+	float r, v;
 	printf("Enter radius =");
 	scanf("%g",&r);
 	v = (float) 4/3 * pi * pow(r,3);
