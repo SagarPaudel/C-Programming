e59a336373ac5b074a0711cf82a52f274a40b655
Sagar Paudel <33595161+SagarPaudel@users.noreply.github.com>
2017-12-10 23:57:01 +0545

    Added condition for negative numbers

--- Assignment 2/Factorial of any number.c
+++ Assignment 2/Factorial of any number.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
   int main()
 {
-	int n, i=1, m =1;
+	int n, i=1, factorial=1;
+	printf("Enter the number\n");
 	scanf("%d",&n);
-	while ( i<=n)
+	
+	if(n<0) //for negative integers
+		printf("Factorial of negative numbers doesn't exists\n");
+	else //for positive integers
 	{
-	m=i*m;	
-	i++;
+		for(i=1;i<=n;i++)
+		{
+			factorial*=i;
+		}
+		printf("The factorial of %d is = %d\n",n,factorial);
 	}
-	printf("%d! is = %d\n",n,m);
-
-}
+	  return 0;
+  }
