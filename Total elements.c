1d82e48c43aa7b8d0d14f39ee450f6b04ef9d8f4
Sagar Paudel <33595161+SagarPaudel@users.noreply.github.com>
2018-02-10 12:22:42 +0545

    Add files via upload

---
+++ Total elements.c
@@ -0,0 +1,10 @@
+// To count total number of elements entered
+#include<stdio.h>
+int main(){
+	int num,i,count=0;
+	printf("enter number\n");
+	scanf("%d",&num);
+	while(num!=0)
+	printf("total num is =%d\n",num);
+	return 0;
+}
